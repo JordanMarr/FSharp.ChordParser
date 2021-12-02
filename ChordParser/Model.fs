9e6adcdd50c0a602d6bf9c4ce7bd9fb772f64243
Jordan Marr <jmarr@microdesk.com>
2021-12-02 12:24:18 -0500

    Fixed so that preferred accidental is set even when transpose = 0

--- ChordParser/Model.fs
+++ ChordParser/Model.fs
@@ -12,29 +12,26 @@ type Chord =
 
 /// Transposes the root note of a chord and optional bass note to the given number of semitones, using the preferred accidental (# or b).
 let transpose semitones preferredAccidental chord =
-    if semitones = 0 
-    then chord
-    else
-        let sharps = rootNotes |> List.filter (fun n -> not (n.EndsWith "b"))
-        let flats = rootNotes |> List.filter (fun n -> not (n.EndsWith "#"))
-        let notes  = 
-            match preferredAccidental with 
-            | "#" -> sharps 
-            | "b" -> flats
-            | _ -> failwith "Preferred Accidental must be either # or b."
+    let sharps = rootNotes |> List.filter (fun n -> not (n.EndsWith "b"))
+    let flats = rootNotes |> List.filter (fun n -> not (n.EndsWith "#"))
+    let notes  = 
+        match preferredAccidental with 
+        | "#" -> sharps 
+        | "b" -> flats
+        | _ -> failwith "Preferred Accidental must be either # or b."
 
-        let transposeNote note =
-            let sharpIdx = sharps |> List.tryFindIndex(fun n -> n = note)
-            let flatIdx = flats |> List.tryFindIndex(fun n -> n = note)
-            let idx = sharpIdx |> Option.orElse flatIdx |> Option.defaultValue -1
-            if idx = -1 then failwith $"Invalid note: '{note}'"
-            let transposedIdx = (idx + semitones) % 12
-            let transposedIdx = if transposedIdx < 0 then transposedIdx + 12 else transposedIdx
-            notes.[transposedIdx]
+    let transposeNote note =
+        let sharpIdx = sharps |> List.tryFindIndex(fun n -> n = note)
+        let flatIdx = flats |> List.tryFindIndex(fun n -> n = note)
+        let idx = sharpIdx |> Option.orElse flatIdx |> Option.defaultValue -1
+        if idx = -1 then failwith $"Invalid note: '{note}'"
+        let transposedIdx = (idx + semitones) % 12
+        let transposedIdx = if transposedIdx < 0 then transposedIdx + 12 else transposedIdx
+        notes.[transposedIdx]
 
-        { chord with 
-            Root = chord.Root |> transposeNote
-            BassNote = chord.BassNote |> Option.map transposeNote }
+    { chord with 
+        Root = chord.Root |> transposeNote
+        BassNote = chord.BassNote |> Option.map transposeNote }
 
 /// Prints chord to string.
 let printChord chord =
