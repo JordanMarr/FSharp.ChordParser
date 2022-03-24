fa81733e602c7ef0683ddb05bb6f2e8bff00fcbf
Jordan Marr <jmarr@microdesk.com>
2022-03-24 11:29:50 -0400

    Changes to console app

--- ChordParser.Console/Program.fs
+++ ChordParser.Console/Program.fs
@@ -1,17 +1,18 @@
 ﻿module ChordParser.Console
 
+open System.IO
 open FSharp.SystemCommandLine
 
-let run (filepath: string, semitones: int, preferredAccidental: string) =
-    filepath
+let run (chordChart: FileInfo, semitones: int, preferredAccidental: string) =
+    chordChart.FullName
     |> System.IO.File.ReadAllText
     |> App.processText semitones preferredAccidental true
-    |> App.saveOutput filepath
+    |> App.saveOutput chordChart.FullName
 
 [<EntryPoint>]
 let main argv =
 
-    let filepath = Input.Argument("filepath", "Path to the input song text file..")
+    let filepath = Input.Argument("filepath", "Path to the input song text file.")
     let semitones = Input.Option(["--semitones"; "-s"], 0, "The number of semitones to transpose (+/-).")
     let preferredAccidental = Input.Option(["--preferred-accidental"; "-a"], "b", "# or b (defaults to b)")
 
