80725426488c62bde7c68fc34b00fad6921d5aa6
Jordan Marr <jmarr@microdesk.com>
2022-03-24 00:38:32 -0400

    Updated to latest FSharp.SystemCommandLine

--- ChordParser.Console/Program.fs
+++ ChordParser.Console/Program.fs
@@ -11,9 +11,9 @@ let run (filepath: string, semitones: int, preferredAccidental: string) =
 [<EntryPoint>]
 let main argv =
 
-    let filepath = Input.Argument<string>("filepath", "Path to the input song text file..")
-    let semitones = Input.Option(["--semitones"; "-s"], (fun () -> 0), "The number of semitones to transpose (+/-).")
-    let preferredAccidental = Input.Option(["--preferred-accidental"; "-a"], (fun () -> "b"), "# or b (defaults to b)")
+    let filepath = Input.Argument("filepath", "Path to the input song text file..")
+    let semitones = Input.Option(["--semitones"; "-s"], 0, "The number of semitones to transpose (+/-).")
+    let preferredAccidental = Input.Option(["--preferred-accidental"; "-a"], "b", "# or b (defaults to b)")
 
     rootCommand argv {
         description "Chord Parser"
